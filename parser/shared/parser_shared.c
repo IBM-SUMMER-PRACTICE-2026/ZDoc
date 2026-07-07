9d0c5a782a98ce7184ad57ba4bf74913548ee1dd
anastassow <127148449+anastassow@users.noreply.github.com>
2026-07-07 16:33:50 +0300

    Added the arry for the output of the parsers

--- parser/shared/parser_shared.c
+++ parser/shared/parser_shared.c
@@ -1,5 +1,10 @@
 #include "parser_shared.h"
 
 int init_resources() {
-    return number_of_files(&global_file_table);
+    if (!number_of_files(&global_file_table)) return 0;
+    else return -1;
+
+    global_parsed_files_arry = malloc(sizeof(TempName) * files_count);
+    
+    return global_parsed_files_arry ? 0 : -1;
 }
--- parser/shared/parser_shared.h
+++ parser/shared/parser_shared.h
@@ -1,6 +1,7 @@
 #ifndef PARSER_SHARED_H
 #define PARSER_SHARED_H
 
+#include <stdlib.h>
 #include <stdatomic.h>
 #include "../../extractor/doc_extractor/module_tree/modtree_tables.h"
 
@@ -9,8 +10,19 @@ extern modtree_dir_table_t global_dir_table;
 extern modtree_file_table_t global_file_table;
 extern int files_count = 0;
 
-/* Function that initializes the filres count,
-    getting it from the interface that modtree_tables.h provides.
+extern TempName* global_parsed_files_arry = NULL;
+
+/* Structure for the global format of output of the parsers.
+    (For now this is example name name 
+    and it is full of garbage just for the test.)
+*/
+typedef struct{
+    int testVar;
+} TempName;
+
+/* Function that initializes the files count,
+    getting it from the interface that modtree_tables.h provides
+    and allocates memory for the global_parsed_files_arry.
     returns 0 on succsess
     returns -1 on failure
 */
