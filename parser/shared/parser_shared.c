58c29638e4b27dd4dbdd115c708b2132c174bdf7
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-07-08 12:22:45 +0300

    Shared structure definition

--- parser/shared/parser_shared.c
+++ parser/shared/parser_shared.c
@@ -4,14 +4,14 @@ _Atomic int finished_files = 0;
 modtree_dir_table_t global_dir_table;
 modtree_file_table_t global_file_table;
 int files_count = 0;
-TempName* global_parsed_files_arry = NULL;
+Module* global_parsed_files_arry = NULL;
 
 int init_resources() {
     int count = number_of_files(&global_file_table);
     if (count < 0) return -1;
     files_count = count;
 
-    global_parsed_files_arry = malloc(sizeof(TempName) * files_count);
+    global_parsed_files_arry = malloc(sizeof(Module) * files_count);
 
     return global_parsed_files_arry ? 0 : -1;
 }
--- parser/shared/parser_shared.h
+++ parser/shared/parser_shared.h
@@ -5,19 +5,43 @@
 #include <stdatomic.h>
 #include "../../extractor/doc_extractor/module_tree/modtree_tables.h"
 
-/* Structure for the global format of output of the parsers.
-    (For now this is example name name
-    and it is full of garbage just for the test.)
-*/
-typedef struct{
-    int testVar;
-} TempName;
+
+// Function input parameter
+typedef struct {
+    char *name;
+    char *description;
+} InputParam;
+
+
+// Parsed informaton for each symbol.
+typedef struct {
+    char *name;
+    char *description;
+    char *signature;
+    InputParam *input;
+    int inputCount;
+    char *output;
+    char *notes;
+    uint32_t line;
+    char *type;
+    char *diagram; // NULL until online mode
+} Symbol;
+
+
+// Module - the information for each file and the array of symbols for it.
+typedef struct {
+    char *filename;
+    Symbol *symbols;
+    int symbolCount;
+    int symbolCap;
+} Module;
+
 
 extern _Atomic int finished_files;
 extern modtree_dir_table_t global_dir_table;
 extern modtree_file_table_t global_file_table;
 extern int files_count;
-extern TempName* global_parsed_files_arry;
+extern Module* global_parsed_files_arry;
 
 /* Function that initializes the files count,
     getting it from the interface that modtree_tables.h provides
@@ -25,6 +49,7 @@ extern TempName* global_parsed_files_arry;
     returns 0 on succsess
     returns -1 on failure
 */
+
 int init_resources();
 
 #endif
