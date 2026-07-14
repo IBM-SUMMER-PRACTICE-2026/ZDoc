e7f2d0092804a26166beea6e1f5d8c1044f5d4be
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-07-14 19:20:02 +0300

    Add bob cli option

--- ai-bob/bob-client.h
+++ ai-bob/bob-client.h
@@ -23,7 +23,7 @@ enum ZDoc_Error append_diagrams(const char * response, size_t response_len, Modu
 enum ZDoc_Error build_bob_prompt(const char * path, Module * module, char ** prompt);
 
 // Makes the call to Bob CLI
-enum ZDoc_Error bob_call(const char * prompt, char ** response, size_t response_len);
+enum ZDoc_Error bob_call(const char * prompt, char ** response, size_t response_len, char * bob_cli);
 
 // Staple a diagram to its symbol
 enum ZDoc_Error append_diagram_to_symbol(const char * diagram, size_t diagram_len, Symbol * symbol);
